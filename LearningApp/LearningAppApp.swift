770b7ee4d31e10c9e8bfd75814ef5f643e18f441
Stéphane Visconti <info@svrai.com>
2021-05-24 08:48:20 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Stéphane Visconti on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
